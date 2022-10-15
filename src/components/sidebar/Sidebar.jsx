95bb80710e4f0a1c68ff16cbbd9d7779f31586c4
Alex-Gichau <gichaumburu@gmail.com>
2022-10-15 08:53:47 +0300

    complete sidenav-navbar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,77 @@
+import './sidebar.scss';
+import {AccountCircleRounded, CreditCard, Dashboard, InsertChartOutlined, LocalShippingOutlined, LogoutRounded, NotificationsNoneSharp, PersonOutline, PsychologyAltOutlined, Settings,SettingsSystemDaydream, StoreMallDirectoryOutlined } from '@mui/icons-material';
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='top'>
+                <span className='logo'>PCEA Juja Township</span>
+            </div>
+            <hr />
+            <div className='center'>
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <Dashboard className='icon'/>
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <PersonOutline className='icon'/>
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <StoreMallDirectoryOutlined className='icon'/>
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <CreditCard className='icon'/>
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingOutlined className='icon'/>
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <InsertChartOutlined className='icon'/>
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneSharp className='icon'/>
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydream className='icon'/>
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyAltOutlined className='icon'/>
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <Settings className='icon'/>
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleRounded className='icon'/>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutRounded className='icon'/>
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className='bottom'>
+                <div className='colorOption'></div>
+                <div className='colorOption'></div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
