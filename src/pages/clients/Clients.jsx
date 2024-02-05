49bc88c61c3721bc0aa53600aed1cd449842e920
Alex Gichau <gichaumburu@gmail.com>
2024-02-05 22:08:20 +0300

    client form

--- src/pages/clients/Clients.jsx
+++ src/pages/clients/Clients.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./clients.css"
 
 export const Clients = () => {
   return (
-    <div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf32YZD_1VPTZHMRxvrM9WFGbadFhjgFMjm6c5eh0P9qE9f4g/viewform?embedded=true" width="640" height="1946" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    <div className='addClientForm'>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf32YZD_1VPTZHMRxvrM9WFGbadFhjgFMjm6c5eh0P9qE9f4g/viewform?embedded=true" width="640" height="2050" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   )
 }
